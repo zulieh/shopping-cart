d05c7e53d7afc2eb5d85c6304222628d4bcf3804
zulieh <tzuleiha@gmail.com>
2023-12-09 14:52:35 +0100

    addCart

--- app.js
+++ app.js
@@ -35,12 +35,13 @@ const addDataToHTML = () => {
 }
 productListHTML.addEventListener('click', (event) => {
     let clickPosition = event.target;
-    if(clickPosition.classList.contain('addCart')){
+    if(clickPosition.classList.contains('addCart')){
         let product_id = clickPosition.parentElement.dataset.id;
         addToCart(product_id);
 
     }
 })
+
 const addToCart = (product_id) => {
     let productInCart = carts.findIndex((value) => value.product_id == product_id)
     if(carts.length <= 0){
@@ -54,37 +55,40 @@ const addToCart = (product_id) => {
             quantity: 1
         })
     }else {
-        carts[productInCart].quantity =   carts[productInCart].quantity + 1;
+        carts[productInCart].quantity =  carts[productInCart].quantity + 1;
     }
     addCartToHTML();   
 }
-const addCartToHTML = () =>{
+const addCartToHTML = () => {
     cartListHTML.innerHTML = '';
     let totalQuantity = 0;
     if(carts.length > 0){
-        carts.forEach(cart =>{
+        carts.forEach(cart => {
             totalQuantity = totalQuantity + cart.quantity;
             let newCart = document.createElement('div');
             newCart.classList.add('item')
     let productPosition = productList.findIndex((value) => value.id == cart.product_id)
-    let info = productList[productPosition]
+    let info = productList[productPosition];
             newCart.innerHTML = `
-            <div class="images">
-            <img src=${info.image} alt="">
-                </div>
-                <div class="name">${info.name}</div>
-                <div class="totalPrice">£${info.price * cart.quantity}</div>
-                <div class="quantity">
-                    <span class="minus"><</span>
-                    <span>${cart.quantity}</span>
-                    <span class="plus">></span>
-                </div>   
-            `
+            <div class="image">
+                <img src="${info.image}" alt="">
+            </div>
+            <div class="name">
+                ${info.name}
+            </div>
+            <div class="totalPrice">
+                £${info.price * cart.quantity}
+            </div>
+            <div class="quantity">
+                <span class="minus"><</span>
+                <span>${cart.quantity}</span>
+                <span class="plus">></span>
+            </div>   
+            `;
             cartListHTML.appendChild(newCart);
         })   
     }
-    // cartIconSpan.innerText = totalQuantity;
-
+    spanCart.innerText = totalQuantity;
 }
 
 const initApp = () => {
@@ -92,7 +96,7 @@ const initApp = () => {
     .then(response => response.json())
     .then(data =>{
         productList = data;
-        console.log(productList);
+        addDataToHTML();
     })
 
 }
